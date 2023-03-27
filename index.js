6771aba22100b9a4e2cdc97d1d8d639f66cdfdf5
Erickkirui <erickkirui653@gmail.com>
2023-03-27 10:12:59 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+// function to move the block left when press left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  // event listener with the above function as a callback
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+// moving the block Right 
+function  moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+   // event listener
+   document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
